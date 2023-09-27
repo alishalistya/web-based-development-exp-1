20b3a095c80fec97c84dec67a4060030353e48ca
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-09-27 13:20:48 +0700

    feat: add Database Wrapper

--- src/app/core/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Controller
-{
-    public function view($folder, $view, $data = []): void
-    {
-        require_once __DIR__ . "/../views/$folder/$view.php";
-    }
-}
---
+++ src/app/views/NotFound/NotFoundView.php
@@ -0,0 +1,14 @@
+<?php
+
+class NotFoundView
+{
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function render()
+    {
+        require_once
+    }
+}
