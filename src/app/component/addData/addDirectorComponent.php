35fa2dbee66f1d90c51575aaa157d24bdad7c8ec
Alisha Listya <alishalistya@Alishas-MacBook-Pro.local>
2023-10-04 14:21:34 +0700

    fix: add page

--- src/app/component/addData/addDirectorComponent.php
+++ src/app/component/addData/addDirectorComponent.php
@@ -2,17 +2,33 @@
     <h1 class="auth-title">
         Tambahkan Director Baru
     </h1>
+    <!-- Name -->
     <div class="form-group">
-        <label for="title">Nama</label>
-        <input class="form-input" name="title" type="text" placeholder="Nama Film" />
+        <label for="title">Name</label>
+        <input class="form-input" name="title" type="text" placeholder="Nama Director" />
     </div>
+
+    <!-- Description -->
     <div class="form-group">
-        <label for="description">Deskripsi</label>
+        <label for="description">Description</label>
         <input class="form-input" name="description" type="text" placeholder="Sebuah Deskripsi"/>
     </div>
+
+    <!-- Birthdate -->
+    <div class="form-group">
+        <label for="birthdate">Birthdate</label>
+        <input class="form-input" name="birthdate" type="date" placeholder="Sebuah Deskripsi"/>
+    </div>
+
+    <!-- Image -->
+    <div class="form-group">
+        <label for="photo">Photo</label>
+        <input class="form-input" name="photo" type="file" placeholder="Sebuah Deskripsi"/>
+    </div>
+
     <button class="btn btn-primary">Submit</button>
 </form>
 
-<div class="register-btn">
+<!-- <div class="register-btn">
     <a href="/auth/register" class="btn alt-btn">tombol lain</a>
-</div>
+</div> -->
