337b8c5ead9b14652604fe67cddb73c99d6776b7
Alisha Listya <alishalistya@Alishas-MacBook-Pro.local>
2023-10-07 19:02:54 +0700

    feat: authentication fe responsive

--- src/app/component/auth/AuthComponent.php
+++ src/app/component/auth/AuthComponent.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link rel="stylesheet" type="text/css" href="/styles/others/main.css">
     <link rel="stylesheet" type="text/css" href="/styles/others/auth.css">
