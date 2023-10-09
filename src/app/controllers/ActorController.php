668872eeae811856734a080a88d2e98fce6109ec
Alisha Listya <alishalistya@Alishas-MBP.lan>
2023-10-09 09:52:42 +0700

    feat: delete director and actor controller API

--- src/app/controllers/ActorController.php
+++ src/app/controllers/ActorController.php
@@ -210,4 +210,35 @@ class ActorController
             http_response_code($e->getCode());
         }
     }
+
+    public function delete() {
+        try {
+            // var_dump($_SERVER['REQUEST_METHOD']);
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case "DELETE":
+                    $auth = Utils::middleware("Authentication");
+                    $auth->isAdminLogin();
+
+                    $actorModel = Utils::model('Actor');
+                    if ($actorModel->deleteActor($_GET['actor_id']) > 0){
+                        // var_dump($_POST);
+                        // header('Location: ' ."http://$_SERVER[HTTP_HOST]".  '/movie/catalog');
+                        header('Content-Type: application/json');
+                        echo json_encode(['error' => null ]);
+                    }
+                    exit;
+                    break;
+                default:
+                    throw new Exception('Method Not Allowed', STATUS_METHOD_NOT_ALLOWED);
+            }
+        } catch (Exception $e) {
+            if ($e->getCode() === STATUS_UNAUTHORIZED) {
+                header("Location: http://localhost:8080/user/login");
+            } else {
+                http_response_code($e->getCode());
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+        }
+    }
 }
